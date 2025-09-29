3c275c0bd60a7c606daae01fbb68689f4fe70a8b
alinkenh <alinkenh@Andrews-MacBook-Air-47.local>
2025-09-29 15:48:33 -0400

    updated printing and function calling

--- RK4LimelightNumIntegrator.m
+++ RK4LimelightNumIntegrator.m
@@ -9,14 +9,14 @@ farAlt = -609.6; %(m)
 appogee = -16741; %(m)
 
 % Inputs
-u = 0; % x component of COM velo. body coordinate system (m/s)
+u = 50; % x component of COM velo. body coordinate system (m/s)
 w = 0; % z component of COM velo. body coordinate system (m/s)
-q = 1; % y component of rotation rate body coordinate syatem (rad/s)
-theta = pi/4; % picth (rad)
-psi = pi/4;
+q = 0; % y component of rotation rate body coordinate syatem (rad/s)
+theta = 0; % pitch (rad)
 xp = 0; % x position in global coordinate system (m)
-zp = farAlt + appogee; % z position in global coordinate system (m)
-t =0 ;
+zp = appogee; % z position in global coordinate system (m)
+t = 0 ;
+rho = 1.225; % air density
 
 % Wind for getVw
 percentage = '50';
@@ -28,12 +28,11 @@ rArea = 2.368; % (m^2)
 D = 7.4; % (m)
 
 % Inital state vector
-state0 = [xp, zp, u, w, q, theta, psi];
+state0 = [xp, zp, u, w, q, theta];
 state = state0;
 
 %Time after appogee recovery bay is deployed
-timeAfterAppogee = 0.5; %(s)
-
+timeAfterAppogee = 15; %(s)
 
 %Integrating for freefall
 
@@ -41,36 +40,18 @@ i = 1;
 time(i,1) = t;
 
 %freefall
-while t <= timeAfterAppogee
-    flightmode = 1;
-    dt = 0.05;
-    state(i+1,:) = RK4Solver(state(i,:),dt,percentage,mass,Iyy,flightmode);
+while state(end,2) < farAlt
+    dt = 0.01;
+    state(i+1,:) = RK4Solver(state(i,:),dt,percentage,mass,Iyy);
     t = t+dt;
     i = i+1;
     time(i,1) = t;
 end
 
-%initial pilot chute deployment
-while t < timeAfterAppogee + 2
-    flightmode = 2;
-    dt = 0.001;
-    state(i+1,:) = RK4Solver(state(i,:),dt,percentage,mass,Iyy,flightmode);
-    t = t +dt;
-    i = i+1;
-    time(i,1) = t;
-end
-
-%fall back down after parachute deployment
-while state(i,2) < 0
-    flightmode = 2;
-    dt = 0.01;
-    state(i+1,:) = RK4Solver(state(i,:),dt,percentage,mass,Iyy,flightmode);
-    t = t +dt;
-    i = i+1;
-    time(i,1) = t;
-end
-
-plot(time,-state(:,2));
+plot(time(1:end),-state(1:end,2));
 ylabel('Altitude (m)')
 xlabel('Time (s)')
 title('Altitude vs Time')
+
+disp(['Terminal velocity in pilot chute phase is: ',num2str(max(state(:,4))) , ' m/s'])
+disp(['Limelight descended ',num2str(-1*(-state(end,2)+state(1,2))), ' m under pilot chute flight phase'])
